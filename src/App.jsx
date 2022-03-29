8e686a02af66abcbb2c92bab88dfbd43c1f6b3c6
drishtipeshwani <archiepeshwani09@gmail.com>
2022-03-29 10:49:56 +0530

    Auth handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import React, {useState,useEffect} from 'react'
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Signup from './components/signUp/SignUp';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login';
+import web3 from './utils/web3'
 
 function App() {
+
+
+    const [wallet, setWallet] =useState(localStorage.getItem('user-wallet'));
+    const [authed, setAuthed] = useState(false);
+
+    useEffect(() => {
+     if(wallet){
+        setAuthed(true);
+     }
+  },[wallet])
+  
+
   return (
     <div className="app">
        <h1>Cross Chain Wallet</h1>
        <Routes>
-        <Route path="/" element={<Signup/>}/>
-        <Route path="dashboard" element={<Dashboard/>} />
-      </Routes>
+         {console.log(authed)}
+         <Route path = '/' element = {authed ? <Login/> : <Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>} />
+       </Routes>
     </div>
   );
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import web3 from '../../utils/web3'
 
 function Dashboard() {
+
+  const [wallet, setWallet] = React.useState(null);
+  
+
+  React.useEffect(() => {
+    setWallet(web3.eth.accounts.wallet.load(localStorage.getItem('password'),'user-wallet'));
+  },[])
+
   return (
     <div>
     <h1>Dashboard</h1>
+    {console.log(wallet)}
     </div>
   )
 }
